771b34ed2f7f3a61d98aace10e999197c3cdfb7a
Yujie Zhu <iceyujie.zhu@mail.utoronto.ca>
2022-10-02 03:30:28 -0400

    This is a good version.

--- src/main/java/BagMain.java
+++ src/main/java/BagMain.java
@@ -16,11 +16,11 @@ class BagMain {
      */
     public static void enhanceBags(Bag[] bags, boolean double_enhance_handbags) {
         // TODO: Implement this.
-        for (Bag bag : bags){
+        for (Bag bag : bags) {
+            bag.enhance();
             if (double_enhance_handbags && bag instanceof HandBag) {
                 bag.enhance();
-                bag.enhance();
-            } else bag.enhance();
+            }
         }
     }
 
